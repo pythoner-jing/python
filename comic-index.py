1ceb08d24424dc381d969f55c1f3385a5cc9f468
JING <jing@jing-Aspire-4352.(none)>
2013-12-21 20:20:12 +0800

    分析漫画首页信息(删除了漫画简介分析)

--- comic-index.py
+++ comic-index.py
@@ -3,7 +3,7 @@
 
 '''
 解析漫画主页
-包括漫画简介、章回、作者、连载状态、分类、漫画名、最新更新
+包括章回、作者、连载状态、分类、漫画名、最新更新
 '''
 
 import urllib2, urllib, re
@@ -11,8 +11,6 @@ import urllib2, urllib, re
 url_index = "http://manhua.dmzj.com/lxjgs/" 
 
 
-#过滤漫画简介
-regex = re.compile(r"<meta\sname=\'description\'\scontent=\"([^\"]+)\"")
 #过滤作者
 regex2 = re.compile(r"<td><a\shref=\'[^\']+\'>(.+)</a><br/></td>")
 #过滤漫画名
@@ -21,40 +19,41 @@ regex3 = re.compile(r"g_comic_name\s=\s\"([^\"]+)\"")
 regex4 = re.compile(r"<td><a\shref=\"[^\"]+\"\salt=\"[^\"]+\">(.+)</a></td>")
 #过滤分类
 regex5 = re.compile(r"<td><a\stitle=\'[^\']+\'\shref=\'[^\']+\'>(.+)</a></td>")
-#过滤最新更新
+#过滤新添章回
 regex6 = re.compile(r"<li><a\stitle=\"[^\"]+\"\shref=\"([^\"]+)\"\s+class=\"color_red\">(.+)</a></li>")
 #过滤章回
 regex7 = re.compile(r"<li><a\stitle=\"[^\"]+\"\shref=\"([^\"]+)\"\s*>(.+)</a></li>")
+#过滤最新章回
+regex8 = re.compile(r"g_last_update\s=\s\"([^\"]+)\"")
+#过滤最新章回
+regex9 = re.compile(r">(.+)<")
 
-url_test = "http://manhua.dmzj.com/jqzd/"
+url_test = "http://manhua.dmzj.com/firefirefire/"
 
 socket = urllib2.urlopen(url_test)
 content = socket.read()
 socket.close()
 
-intro = regex.findall(content)[0]
 author = regex2.findall(content)[0]
 name = regex3.findall(content)[0]
 status = regex4.findall(content)[1]
 category = regex5.findall(content)[0]
-lastupdate = regex6.findall(content)[0]
+update = regex6.findall(content)
+lastupdate = regex8.findall(content)[0]
 chapters0 = regex7.findall(content)
 
-t = "漫画介绍："
-i = intro.find(t)
-
-intro = intro[i + len(t):]
 chapters = []
 for x in chapters0:
 	chapters.append((x[0], x[1]))
-chapters.append((lastupdate[0], lastupdate[1]))
+
+for x in update:
+	chapters.append((x[0], x[1]))
 
 with open("output.txt", "w") as f:
 	f.write("漫画名 - " + name + "\n")
 	f.write("作者 - " + author + "\n")
 	f.write("分类 - " + category + "\n")
-	f.write("漫画简介 - " + intro + "\n")
 	f.write("连载状态 - " + status + "\n")
-	f.write("最新更新 - " + lastupdate[1] + "\n")
+	f.write("最新更新 - " + lastupdate + "\n")
 	for x in chapters:
 		f.write(" ".join((x[0], x[1])) + "\n")
